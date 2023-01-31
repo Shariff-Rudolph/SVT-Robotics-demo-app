b3d9616a166651e7236a42e122508f0498a345fd
Shariff Rudolph <reefness@Shariffs-MacBook-Pro.local>
2023-01-30 21:01:33 -0500

    declare variable to target table base

--- src/robots.js
+++ src/robots.js
@@ -4,6 +4,7 @@ const getRobots = async () => {
   try {
     const res = await axios.get("https://60c8ed887dafc90017ffbd56.mockapi.io/robots");
     const arr = res.data;
+    const newTable = document.getElementById("table");
   } catch(e){
     console.log("ERROR!", e);
   }
